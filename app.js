9a98ed7d29f48cb41addc6014410f62d4851ceb0
Esther <fuest8456258@gmail.com>
2022-08-16 22:30:40 +0800

    2022081601

--- app.js
+++ app.js
@@ -398,12 +398,12 @@ class Updata {
                 // loadingPage.classList.add('complete')
                 clearInterval(timer)
             }
-        },50)
+        },100)
     }
 
     Animation() {
         tl.call(this.loadingcssAni)
-          .to(".coffee-cup",{rotation: 360,scale: 0,duration: 1},'>7.5')
+          .to(".coffee-cup",{rotation: 360,scale: 0,duration: 1},'>9')
           .to(".loading-text",{scale: 0,duration: 1},'<')  // '<'在上一個動畫 開始處 插入此動畫   與上一個動畫一起開始
           .to(".loadingPage",{y:"200%","display": "none",duration: .5})
           .call(this.homeAni)
